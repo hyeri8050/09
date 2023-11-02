92da7d55e5de18eb542f51a53589dceae199ab2c
Hyeri Park <phr66@naver.com>
2023-11-02 22:32:16 +0900

    02-1

--- main.c
+++ main.c
@@ -7,15 +7,11 @@ int main(int argc, char *argv[])
 {
   int i;
   int grade[N_STUDENT];
+  printf("5명의 점수를 입력하세요.\n");
   
-  /*for ( i=0; i<N_STUDENT; i++)
-        grade[i] = (i+1)*10; */
+  for (i=0; i<N_STUDENT; i++)
+      scanf("%d", &grade[i]);
   
-  grade[0] = 10;
-  grade[1] = 20;
-  grade[2] = 30;
-  grade[3] = 40;
-  grade[4] = 50;
   
   for (i=0; i<N_STUDENT; i++)
   {
